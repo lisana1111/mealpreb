125457969b07a610278f249782fbc48de2807155
lisana1111 <lina.mayer@gmx.ch>
2025-03-10 19:57:49 +0100

    weeklyplaner ausgefüllt, mit random rezepten

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
  
   def index
     @planer = Weeklyplaner.first
+    recipesample = Recipe.all.sample(7)
+    @planer.monday_recipe = recipesample[0]
+    @planer.tuesday_recipe = recipesample[1]
+    @planer.wednesday_recipe = recipesample[2]
+    @planer.thursday_recipe = recipesample[3]
+    @planer.friday_recipe = recipesample[4]
+    @planer.satureday_recipe = recipesample[5]
+    @planer.sunday_recipe = recipesample[6]
   end 
+
+
 end
